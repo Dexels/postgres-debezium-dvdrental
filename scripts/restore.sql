76d4c9be3f0d0bb225a79b1cf25c1b4f73be71dc
Frank Lyaruu <frank@dexels.com>
2020-02-21 17:35:35 +0100

    fix whitespace issues

--- scripts/restore.sql
+++ scripts/restore.sql
@@ -769,7 +769,7 @@ CREATE TABLE staff (
     last_name character varying(45) NOT NULL,
     address_id smallint NOT NULL,
     email character varying(50),
-    salary smallint NOT NULL,
+    salary smallint,
     store_id smallint NOT NULL,
     active boolean DEFAULT true NOT NULL,
     username character varying(16) NOT NULL,
